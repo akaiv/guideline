4fd81bcbae6c817ee7d8c6ffd8de73ac328b3d7b
s10n <a@akaiv.com>
2014-12-08 01:56:57 +0900

    [스타일] 기초 스타일 작성: 배경색, 제목과 글자 크기, 상자

--- templates/content-data.php
+++ templates/content-data.php
@@ -3,10 +3,10 @@ $url  = akaiv_get_post_meta( 'url' );
 $desc = akaiv_get_post_meta( 'data-description' );
 
 if ( ! empty($url) ) : ?>
-  <a href="<?php echo $url; ?>" target="_blank"><?php the_title(); ?></a><?php
+  <a href="<?php echo $url; ?>" target="_blank"><?php akaiv_the_title(); ?></a><?php
   how_data_url_host_name( $url );
 else :
-  the_title();
+  akaiv_the_title();
 endif;
 
 if ( $desc ) : ?>
