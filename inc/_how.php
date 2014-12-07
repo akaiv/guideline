59205048bb3c7ccc73a37719dacaa859a2cc2a82
s10n <a@akaiv.com>
2014-12-07 23:16:10 +0900

    함수: 이 프로젝트에서 사용할 함수 파일 생성하고 자료 URL의 호스트 이름을 출력하는 함수 작성

---
+++ inc/_how.php
@@ -0,0 +1,35 @@
+<?php
+/* 자료 URL의 호스트 이름 */
+function how_data_url_host_name( $url ) {
+  $host_name = how_get_data_url_host_name( $url );
+  if ( ! empty($host_name) )
+    echo ' <span class="text-light">&#124; ' . $host_name . '</span>';
+}
+function how_get_data_url_host_name( $url ) {
+  $host = parse_url($url)['host'];
+  if ( array_key_exists('path', parse_url($url)) )
+    $path = parse_url($url)['path'];
+  if ( isset($path) && '/' === $path )
+    $path = '';
+
+  if ( false !== strpos($host, 'wikipedia.org') ) :
+    $host_name = '위키피디아';
+  elseif ( 'ref.akaiv.com' === $host && ! empty($path) ) :
+    $host_name = '디자인 레퍼런스 아카이브';
+  elseif ( 'tech.akaiv.com' === $host && ! empty($path) ) :
+    $host_name = '기술 지식 아카이브';
+  elseif ( false !== strpos($host, 'google.com') ) :
+    if ( strpos($path, 'design') && '/design/' !== $path ) :
+      $host_name = '구글 디자인 가이드라인';
+    elseif ( false !== strpos($host, 'developers') ) :
+      $host_name = 'Google Developers';
+    endif;
+  elseif ( false !== strpos($host, 'apple.com') ) :
+    $host_name = 'iOS 휴먼 인터페이스 가이드라인';
+  elseif ( false !== strpos($host, 'codex.wordpress.org') && ! empty($path) ) :
+    $host_name = 'Codex';
+  endif;
+
+  if ( ! empty($host_name) )
+    return $host_name;
+}
