9942dafd05cf3256d63d591e66597cfdfd39f2f3
s10n <a@akaiv.com>
2014-12-10 22:58:09 +0900

    템플릿: 자료의 카테고리 제목 태그를 <ul> 밖에서 선언

--- templates/loop-data.php
+++ templates/loop-data.php
@@ -10,15 +10,17 @@ $args = array (
 );
 $data = new WP_Query( $args );
 
-if ( $data->have_posts() ) : ?>
-  <ul><?php
-    while ( $data->have_posts() ) : $data->the_post();
-      $cat = akaiv_get_post_meta( 'data-cat' );
-      if ( ! isset($current_cat) || $current_cat !== $cat )
-        echo '<h3>'.$cat.'</h3>'; ?>
-      <li><?php get_template_part( 'templates/content', get_post_type() ); ?></li><?php
-      $current_cat = $cat;
-    endwhile; ?>
-  </ul><?php
+if ( $data->have_posts() ) :
+  while ( $data->have_posts() ) : $data->the_post();
+    $cat = akaiv_get_post_meta( 'data-cat' );
+    if ( ! isset($current_cat) ) :
+      echo '<h3 class="data-cat-title">'.$cat.'</h3><ul>';
+    elseif ( $current_cat !== $cat ) :
+      echo '</ul><h3 class="data-cat-title">'.$cat.'</h3><ul>';
+    endif; ?>
+    <li><?php get_template_part( 'templates/content', get_post_type() ); ?></li><?php
+    $current_cat = $cat;
+  endwhile;
+  echo '</ul>';
 endif;
 wp_reset_postdata(); ?>
