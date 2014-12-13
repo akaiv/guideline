ba41f2693d7319ea043e33b1f862a4729cfc5922
s10n <a@akaiv.com>
2014-12-13 16:17:16 +0900

    템플릿: loop-front.php 작성

--- templates/content-front.php
+++ templates/content-front.php
@@ -1,32 +1,3 @@
-<div class="page-content"><?php
-
-  $args = array (
-    'post_type'   => 'guideline',
-    'post_parent' => 0,
-    'orderby'     => 'menu_order',
-    'order'       => 'ASC',
-  );
-  $guidelines = new WP_Query( $args );
-
-  if ( $guidelines->have_posts() ) :
-    $obj = get_post_type_object( 'guideline' );
-    akaiv_page_header( $obj->labels->name ); ?>
-
-    <div class="row"><?php
-      $i = 0;
-      while ( $guidelines->have_posts() ) : $guidelines->the_post(); ?>
-        <div class="col-md-3">
-          <?php get_template_part( 'templates/content', 'guideline' ); ?>
-        </div><?php
-        if ( 3 === $i % 4 && count($guidelines->posts) - 1 !== $i )
-          echo '</div><hr><div class="row">';
-        $i++;
-      endwhile; ?>
-    </div><?php
-
-  else :
-    get_template_part( 'templates/content', 'none' );
-
-  endif;
-  wp_reset_postdata(); ?>
+<div class="page-content">
+  <?php get_template_part( 'templates/loop', 'front' ); ?>
 </div>
